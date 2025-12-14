547b54e0e0a5d1006c84470f9319fbb9b93ccd9c
Hemang Murugan <52290764+iamtheretronerd@users.noreply.github.com>
2025-12-14 09:48:08 -0500

    Fix routing

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(buildPath));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(buildPath, "index.html"));
   });
 }
